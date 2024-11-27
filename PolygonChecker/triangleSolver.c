88ccc80654145566e51534f6011ffbb9284ede67
AE5512 <aenglish5512@conestogac.on.ca>
2024-11-27 12:47:06 -0500

    Triangle function will now give angles

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "triangleSolver.h"
+
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
@@ -21,4 +23,25 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 
 	return result;
+}
+
+void anglePrint(int* input) { //reusable print function
+	double angleA = findingAngle(input[0], input[1], input[2]);
+	double angleB = findingAngle(input[1], input[2], input[0]);
+	// this fixes an edge case that would result in the angle sum being 179
+	// this is meant to make the triangle true even if we lie a little
+	double angleC = 180 - round(angleA) - round(angleB);
+	printf("Sngle a: %.0lf\nAngle b: %.0lf\nAngle c: %.0lf\n", angleA, angleB, angleC);
+
+}
+
+double findingAngle(int a, int b, int c) {
+	double PI = 3.1415926; // a constant for 
+	// arccos[(b^2 + c^2 - a^2) / 2bc]
+	double topHalf = (b*b) + (c*c) - (a*a);
+	double bottomHalf = 2*b*c;
+	double result = topHalf / bottomHalf;
+	// Converting radains into degrees
+	double degree = acos(result) * 180 / PI;
+	return degree;
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,6 @@
 #pragma once
 char* analyzeTriangle(int side1, int side2, int side3);
+
+void anglePrint(int* input); // the function to print off the angles
+
+double findingAngle(int a, int b, int c);
